d92b671d693a9c866efbaa33a9b1b8f7877694ec
Niranjan Godbole <niranjan8192@gmail.com>
2018-02-13 03:55:19 +0530

    remove unused files

--- actions/dashboard.go
+++
@@ -1,9 +0,0 @@
-package actions
-
-import "github.com/gobuffalo/buffalo"
-
-// DashboardHandler is a default handler to serve up
-// a home page.
-func DashboardHandler(c buffalo.Context) error {
-	return c.Render(200, r.HTML("dashboard/dashboard.html"))
-}
